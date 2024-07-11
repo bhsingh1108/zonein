9b6961ab808aef6b1ec5d0b906d6b910748c393a
wavecrypto3 <102922567+wavecrypto3@users.noreply.github.com>
2024-07-11 14:47:25 +0530

    set passport to null

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,11 @@
 exports.postusers= (req, res) => {
     var connection = req.app.get('conn');
+    const last_name=req.body.last_name?req.body.last_name:'';
     const user = {
-        username: req.body.first_name +' '+ req.body.last_name?req.body.last_name:'',
+        username: req.body.first_name +' '+ last_name,
         mobile: req.body.phone_number,
         email: req.body.email,
-        passport_no: req.body.passport_no?req.body.passport_no:''
+        passport_no: req.body.passport_no?req.body.passport_no:'null'
     };
     
     const checkEmailSql = 'SELECT * FROM users WHERE email = ?';
